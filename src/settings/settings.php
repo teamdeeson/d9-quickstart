2531b74d72547866712cff249211d062a7454d94
John Ennew <johne@deeson.co.uk>
2020-07-21 14:59:09 +0100

    Removed install gumpf from settings file

--- src/settings/settings.php
+++ src/settings/settings.php
@@ -14,15 +14,3 @@ require_once dirname(DRUPAL_ROOT) . '/src/settings/environment.inc';
 foreach (glob(dirname(DRUPAL_ROOT) . '/src/settings/*.settings.inc') as $file) {
   require_once $file;
 }
-$databases['default']['default'] = array (
-  'database' => 'drupal',
-  'username' => 'drupal',
-  'password' => 'drupal',
-  'prefix' => '',
-  'host' => 'mariadb',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = '0VwzvcKyOBefR3RiWYGHjMRYiqx0IcJOqmSQ6p0XbVg156GtTqaxhkCCit4SYwcgMSJjjFJ1Jg';
-$settings['config_sync_directory'] = 'sites/default/files/config_CHYnkkYrQBRVFkijl3GbPBXLM3e_QNrnmp8jpe9g-k2QNjwgb2K7FhKGxssFjj0v8Y2sYjRHsw/sync';
