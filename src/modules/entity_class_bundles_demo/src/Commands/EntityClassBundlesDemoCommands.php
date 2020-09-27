35e12e0fc93f7464c8bff5023012d9489966f7c6
Benjamin Dudiak-Fry <bend@deeson.co.uk>
2020-09-27 16:01:03 +0100

    Bugfixes

--- src/modules/entity_class_bundles_demo/src/Commands/EntityClassBundlesDemoCommands.php
+++ src/modules/entity_class_bundles_demo/src/Commands/EntityClassBundlesDemoCommands.php
@@ -15,14 +15,17 @@ class EntityClassBundlesDemoCommands extends DrushCommands {
    * @usage entity_class_bundles_demo:log_article_tags
    */
   public function logArticleTags() {
-    $articles = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type' => 'article', 'status' => 1]);
-    foreach($articles as $article) {
-      if($article instanceof Article) {
-        \Drupal::logger('entity_class_bundles_demo')->notice('Article' . $article->getTitle() . '  has these tags:');
-        $tags = $article->getTags();
-        foreach($tags as $tag) {
-          \Drupal::logger('entity_class_bundles_demo')->notice('--' . $tag->getLoudName());
-        }
+    /* @var \Drupal\entity_class_bundles_demo\Entity\Article[] $articles */
+    $articles = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->loadByProperties(['type' => 'article', 'status' => 1]);
+    foreach ($articles as $article) {
+      \Drupal::logger('entity_class_bundles_demo')
+        ->notice('Article' . $article->getTitle() . '  has these tags:');
+      $tags = $article->getTags();
+      foreach ($tags as $tag) {
+        \Drupal::logger('entity_class_bundles_demo')
+          ->notice('--: ' . $tag->getLoudName());
       }
     }
   }
